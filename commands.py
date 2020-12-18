eb89883082e0a352d1427e7a66773cf4673d67ff
yashsehgal <yashsehgal.work@gmail.com>
2020-12-18 09:20:25 +0530

    design system bot, initial files are added to the project. concept commands are added to (commands.py)

---
+++ commands.py
@@ -0,0 +1,29 @@
+
+
+# command list for the bot
+
+commands = {
+  "github": {
+    "-ds <username>": "To open your github profile",
+    "-ds <username> <repository_name>": "To open particular repository from your github profile"
+  },
+  "documentation": {
+    "-ds docs": "To open the documentation of DesignSystem",
+    "-ds docs <topic>": "To open the documentation for a particular topic"
+  },
+  "general": {
+    "-ds help": "To open the cheat sheet"
+  }
+}
+
+def getCommands():
+  return commands
+
+def getCommandsForGithub():
+  return commands.get("github")
+
+def getCommandsForDocumentation():
+  return commands.get("documentation")
+
+def getCommandsForGeneral():
+  return commands.get("general")
