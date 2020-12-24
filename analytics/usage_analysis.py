1252e8abcb60f241644539e0cfb44d9df81d6af8
yashsehgal <yashsehgal.work@gmail.com>
2020-12-24 20:25:01 +0530

    webpage modules are added to the project

---
+++ analytics/usage_analysis.py
@@ -0,0 +1,46 @@
+'''
+  This module is to perform some analytical features on the dataset
+  Like analysing the command usage and more such functions
+
+  @author: @yashsehgal
+  
+  Features to be implemented
+  
+  TODO:   To make a method which will fetch data items from a file and save inside an instance.
+  TODO:   Specific methods for specific functions. such as, computing command usages and etc. 
+
+  #? To use exception handling methods in all the methods
+  #? To make seprate methods for all the features
+'''
+
+def fetchDataForOperation(data_tag):
+  filename = ""
+  isFileAvailable = False
+  dataset = []
+  
+  if data_tag == "command":
+    # complete analysis of command usage
+    # preparing complete dataset for this feature
+    pass
+    
+  elif data_tag == "portfolio":
+    # complete analysis of portfolios
+    pass
+  
+  elif data_tag == "complete":
+    # complete analytical details of the bot
+    pass
+  
+'''
+  Complete analysis for command operations
+  params: dataset [type: array]
+'''
+
+def analyseDataForCommands(dataset):
+  pass
+
+def analyseDataForCommands(dataset):
+  pass
+
+def analyseCompleteBot(dataset):
+  pass
