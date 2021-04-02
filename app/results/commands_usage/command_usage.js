519b2f0a44721ebd0a234f517f93d3fd4ff5cf98
yashsehgal <yashsehgal.work@gmail.com>
2021-04-03 01:06:00 +0530

    webpage code added with css/scss files

--- app/results/commands_usage/command_usage.js
+++ app/results/commands_usage/command_usage.js
@@ -1,4 +1,4 @@
-
+"use strict";
 
 let command_usage_page = document.createElement("div");
 command_usage_page.className = "page";
@@ -6,9 +6,15 @@ command_usage_page.className = "page";
 let command_usage_status_container = document.createElement("div");
 command_usage_status_container.className = "section command_usage_status_section";
 command_usage_status_container.innerHTML = `
-  <img src="../../../commands_usage_plot.png" />
+  <img src="../../../commands_usage_plot.png" class="status_graph" />
+  <img src="../../../commands_usage_plot.png" class="status_graph" />
 `;
 
 command_usage_page.appendChild(command_usage_status_container);
 
 document.getElementById("root").appendChild(command_usage_page);
+
+
+// var tag = document.getElementsByTagName("p")[0];
+// text = tag.innerHTML;
+// openSomehowPythonInterpreter("./usage_analysis.py", "runThisScript()");
