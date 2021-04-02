0d9a5851c5ac64f1cbced2feb09c81768edc57bb
yashsehgal <yashsehgal.work@gmail.com>
2021-04-02 17:43:35 +0530

    path updated again

--- app/results/commands_usage/command_usage.js
+++ app/results/commands_usage/command_usage.js
@@ -6,7 +6,7 @@ command_usage_page.className = "page";
 let command_usage_status_container = document.createElement("div");
 command_usage_status_container.className = "section command_usage_status_section";
 command_usage_status_container.innerHTML = `
-  <img src="./commands_usage_plot.png" />
+  <img src="../../../commands_usage_plot.png" />
 `;
 
 command_usage_page.appendChild(command_usage_status_container);
